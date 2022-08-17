10ef46296408c841f356d3c933c8a42219603085
Ian Harris <ian@fofgof.xyz>
2022-08-16 20:01:26 -0700

    some localstorage test

---
+++ index.js
@@ -0,0 +1,10 @@
+addEventListener('load', () => {
+  // moneyOnHand = localStorage.getItem('moneyOnHand')
+  // if (moneyOnHand) {
+  //   document.getElementById('moneyOnHand').innerText = moneyOnHand
+  // }
+})
+
+function updateMoneyOnHand() {
+  // localStorage.setItem('moneyOnHand', document.getElementById('moneyOnHand').innerText)
+}
