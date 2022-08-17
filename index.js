0695eb51b8c6933c86eb6b62e08c22fc4a709997
Ian Harris <ian@fofgof.xyz>
2022-08-16 20:53:10 -0700

    use oninput

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener('load', () => {
+window.addEventListener('load', () => {
   moneyOnHand = localStorage.getItem('moneyOnHand')
   if (moneyOnHand) {
     document.getElementById('moneyOnHand').value = moneyOnHand
