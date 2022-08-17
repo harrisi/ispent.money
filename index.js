842b5364358b4687c9852ec4f279255c1cda3574
Ian Harris <ian@fofgof.xyz>
2022-08-16 20:04:30 -0700

    I am a fool.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 addEventListener('load', () => {
-  // moneyOnHand = localStorage.getItem('moneyOnHand')
-  // if (moneyOnHand) {
-  //   document.getElementById('moneyOnHand').innerText = moneyOnHand
-  // }
+  moneyOnHand = localStorage.getItem('moneyOnHand')
+  if (moneyOnHand) {
+    document.getElementById('moneyOnHand').innerText = moneyOnHand
+  }
 })
 
 function updateMoneyOnHand() {
-  // localStorage.setItem('moneyOnHand', document.getElementById('moneyOnHand').innerText)
+  localStorage.setItem('moneyOnHand', document.getElementById('moneyOnHand').innerText)
 }
