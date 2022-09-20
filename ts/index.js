c049dc92bb15e8dfe1528018b350903adc48ed34
Ian Harris <ian@fofgof.xyz>
2022-09-20 14:34:15 -0700

    init ts commit

---
+++ ts/index.js
@@ -0,0 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = "".concat(firstName, " ").concat(middleInitial, " ").concat(lastName);
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, ".concat(person.firstName, " ").concat(person.lastName);
+}
+var user = new Student("Jane", "M.", "User");
+document.body.textContent = greeter(user);
